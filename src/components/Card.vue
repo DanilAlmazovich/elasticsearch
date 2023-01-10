ba7ea2551edded1a49a79dff156a91477e1cfa90
Danil <danil@azor.kg>
2023-01-10 10:16:16 +0600

    First commit

---
+++ src/components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex mb-10 items-center">
+    <div class="w-20 h-20 mr-4">
+      <img class="w-full h-full object-contain"
+           :src="item?.image?.medium || 'https://upload.wikimedia.org/wikipedia/ru/thumb/a/ac/No_image_available.svg/1200px-No_image_available.svg.png'"
+           :alt="item.name">
+    </div>
+    <p class="first-letter:text-blue-500 first-letter:font-semibold">{{ item.name }}</p>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  }
+}
+</script>
